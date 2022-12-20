289dee4d04b801f40c86c6a77642e28b8523545b
utsav82 <utsa.yadav.94@gmail.com>
2022-12-21 00:32:20 +0530

    Changed sample

--- src/data.js
+++ src/data.js
@@ -1,32 +1,8 @@
 export default [
     {
       id: 1,
-      name: 'Bertie Yates',
+      name: 'Sample Name',
       time:'10 AM',
       
-    },
-    {
-      id: 2,
-      name: 'Hester Hogan',
-      time: '11 AM',
-      
-    },
-    {
-      id: 3,
-      name: 'Larry Little',
-      time: '1 PM',
-     
-    },
-    {
-      id: 4,
-      name: 'Sean Walsh',
-      time: '2 PM',
-     
-    },
-    {
-      id: 5,
-      name: 'Lola Gardner',
-      time: '3 PM',
-     
-    },
+    }
   ];
